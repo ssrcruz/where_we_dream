0bc98a2d1d8f96a7f50eff93749e614da375af7d
Ruben <ssrcruz@gmail.com>
2015-05-30 12:43:28 -0400

    Add act_as_mappable to school.rb and Remove Locations table

--- app/models/school.rb
+++ app/models/school.rb
@@ -1,5 +1,6 @@
 class School < ActiveRecord::Base
-
+  acts_as_mappable :auto_geocode=>{:field=>:address, :error_message=>'Could not geocode address'}
+  
   def self.search(search)
     if search
       where(["name LIKE ?","%#{search}%"])
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150510174535) do
+ActiveRecord::Schema.define(version: 20150530163906) do
 
   create_table "schools", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20150510174535) do
     t.integer  "rating"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "address"
   end
 
   create_table "searches", force: :cascade do |t|
